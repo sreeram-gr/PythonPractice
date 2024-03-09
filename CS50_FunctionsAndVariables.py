d23c2cb7dae0a2dfa61f15139b8b9bf8d3f9aac7
Sreeram Govindan Rajendran <sreerambeece@gmail.com>
2024-03-09 22:07:44 +0530

    updating the CS50 samples

---
+++ CS50_FunctionsAndVariables.py
@@ -0,0 +1,13 @@
+#Ask user for the name
+
+name = input("What is my name?")
+
+#Say hello to the user
+print("Hello,")
+print(name)
+
+#print("input, sep=" ",end="\n"")
+
+print("Hello,", name)
+
+print(f"Hello, {name}")
