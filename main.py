1ac5254077685a8a3af9c125a4ecc2068294a8c1
Sreeram Govindan Rajendran <sreerambeece@gmail.com>
2024-08-18 23:55:39 +0530

    Updated the ipynb

--- main.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get("https://api.github.com/users/SreeramGovindanRajendran/repos")
-repos = response.json()
-
-for repo in repos:
-    print(f"Name : {repo['name']}\nURL : {repo['url']}")
